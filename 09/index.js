91dda8c0633927e57f4d7bba64fcdab6c182267b
Elder Gomes <eldergomesdas@gmail.com>
2026-03-04 12:16:23 -0300

    exercicio 7 soma dos angulos

---
+++ 09/index.js
@@ -0,0 +1,8 @@
+const numeroDeLadosDoPoligno =6;
+const somaDosAngulosDoPoligno = (numeroDeLadosDoPoligno - 2) * 180;
+const valorDeCadaAngulo = (somaDosAngulosDoPoligno/numeroDeLadosDoPoligno)
+console.log(`A soma dos lados do poligno é : ${somaDosAngulosDoPoligno} `);
+console.log(`o valor de cada lado é : ${valorDeCadaAngulo}`);
+
+
+ 
