8321714e8add74c4d1c432885b93d8dd01131d2d
Elder Gomes <eldergomesdas@gmail.com>
2026-03-04 13:26:00 -0300

    exercicio 13  area do cilindro

---
+++ 13/index.js
@@ -0,0 +1,8 @@
+const raioDaBase=3;
+const alturaDoCilindro=1;
+const areaTotal = 2*Math.PI*raioDaBase*(raioDaBase+alturaDoCilindro);
+
+console.log(`A área total do Cilindro é : ${areaTotal.toFixed(2)}`);
+
+
+
