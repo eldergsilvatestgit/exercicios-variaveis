d0ec94e8373395efeeb73fb7988720d40b367768
Elder Gomes <eldergomesdas@gmail.com>
2026-03-04 11:27:32 -0300

    exercicio 9 calculo de area

---
+++ 08/index.js
@@ -0,0 +1,6 @@
+const raioDoCirculo=1;
+const areDoCirculo = (raioDoCirculo**2 )*Math.PI;
+const comprimentoDoCirculo =2*Math.PI*raioDoCirculo;
+
+console.log(`O comprimento do circulo é ${comprimentoDoCirculo.toFixed(2)}`);
+console.log(`A are do circulo é ${areDoCirculo.toFixed(2)}`);
