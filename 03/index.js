97c5014db2c6cfecd089aec3b9d106a209be6060
Elder Gomes <eldergomesdas@gmail.com>
2026-03-03 22:25:44 -0300

    exercicio 3

---
+++ 03/index.js
@@ -0,0 +1,10 @@
+
+
+const precoTenis = 129.99;
+const meuDinheiro = 80.00;
+const desconto = (precoTenis  - meuDinheiro );
+const Porcentagem = (desconto * 100) /precoTenis ;
+
+console.log(`Seu desconto é de: ${Porcentagem.toFixed(2)} %`);
+
+ 
