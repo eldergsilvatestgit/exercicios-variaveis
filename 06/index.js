0a8e69cfc6c51cda4424729a38f7db87bcbfa24d
Elder Gomes <eldergomesdas@gmail.com>
2026-03-04 10:08:01 -0300

    exercico 06 velocidade média

---
+++ 06/index.js
@@ -0,0 +1,5 @@
+const distanciaEmMetros = 500;
+const  tempoEmSegundos = 10;
+const velocidadeEmKmPorHora  = distanciaEmMetros/tempoEmSegundos *3.6;
+
+console.log(`Velocidade média: ${velocidadeEmKmPorHora.toFixed(2)} km/h`);
